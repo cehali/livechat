566da276f5b992995807e07133cd5def7e3ddafd
Cezary Haliniarz <cezary@ethworks.io>
2019-11-03 16:13:38 +0100

    Styling

--- src/ui/MainScreen.tsx
+++ src/ui/MainScreen.tsx
@@ -1,16 +1,17 @@
 import React, {useState} from 'react';
 import {useHistory} from 'react-router';
+import '../styles/mainScreen.css'
 
 const MainScreen = () => {
   const [clientName, setClientName] = useState('');
   const history = useHistory();
   
   return (
-    <>
-      <input onChange={(event) => setClientName(event.target.value)}></input>
-      <button onClick={() => history.push('/client', {clientName})}>Client</button>
-      <button onClick={() => history.push('/host')}>Host</button>
-    </>
+    <div className="main-screen">
+      <input className="main-screen-input" placeholder="Select your client username" onChange={(event) => setClientName(event.target.value)} />
+      <button className="main-screen-btn" onClick={() => history.push('/client', {clientName})}>Client</button>
+      <button className="main-screen-btn" onClick={() => history.push('/host')}>Host</button>
+    </div>
   )
 };
 
--- src/ui/commons/Chat.tsx
+++ src/ui/commons/Chat.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Message} from 'src/types/Message';
+import '../../styles/chat.css';
 
 interface ChatProps {
   messages: Message[];
@@ -16,21 +17,19 @@ const Chat = ({messages, onSendMessage, receiverName}: ChatProps) => {
   }
 
   return (
-    <>
-      <div id="header">
-        <p id="username">{receiverName}</p>
-      </div>
-      <div id="messages">
-        {messages.map((message) => message.from === receiverName ?
-          <p className='message-right'>{message.messageText}</p>
-          : <p className='message-left'>{message.messageText}</p>
+    <div className="chat-window">
+      <p id="username" className="chat-header">{receiverName}</p>
+      <div className="messages-box">
+        {messages.map((message) => message.from === receiverName.toLowerCase() ?
+          <p className='message message-left'>{message.messageText}</p>
+          : <p className='message message-right'>{message.messageText}</p>
         )}
       </div>
-      <div id="text-box">
-        <textarea onChange={(event) => setMessageText(event.target.value)} />
-        <button onClick={() => onSend(messageText, receiverName)}>Send</button>
+      <div className="message-text-box">
+        <textarea className="message-text" onChange={(event) => setMessageText(event.target.value)} />
+        <button className="send-btn" onClick={() => onSend(messageText, receiverName)} disabled={!messageText}>Send</button>
       </div>
-    </>
+    </div>
   )
 };
 
